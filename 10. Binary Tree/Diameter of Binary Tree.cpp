305fb432229d4df357772823cd3b61c936f19e03
Anurag kharwar <Anurag.singh.tmk@gmail.com>
2022-07-14 03:51:18 +0530

    treees and BST

---
+++ 10. Binary Tree/Diameter of Binary Tree.cpp
@@ -0,0 +1,24 @@
+class Solution
+{
+  int height(TreeNode *root)
+  {
+    if (root == NULL)
+      return 0;
+    return 1 + max(height(root->left), height(root->right));
+  }
+
+public:
+  int diameterOfBinaryTree(TreeNode *root)
+  {
+    if (root == NULL)
+      return 0;
+    int lh = height(root->left);
+    int rh = height(root->right);
+    //         max lenght path may be different rerooting technique.
+
+    int ld = diameterOfBinaryTree(root->left);
+    int rd = diameterOfBinaryTree(root->right);
+
+    return max(lh + rh, max(ld, rd));
+  }
+};
